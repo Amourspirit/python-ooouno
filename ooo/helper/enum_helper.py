d74823662fcc1d8cd574a7f5acfe18d5f5d831e0
Barry-Thomas-Paul: Moss <4193389+Amourspirit@users.noreply.github.com>
2022-09-21 12:18:24 +0000

    Develop (#19)

--- ooo/helper/enum_helper.py
+++ ooo/helper/enum_helper.py
@@ -1,11 +1,8 @@
 # coding: utf-8
 from __future__ import annotations
 from typing import Any
-import sys
 import uno
 from enum import Enum, EnumMeta, _EnumDict
-
-# coding: utf-8
 
 
 def uno_enum_class_new(cls, value):
@@ -93,6 +90,7 @@ class UnoEnumMeta(type):
         cls.typeName = type_name
         cls.__ooo_full_ns__ = type_name
         cls.__ooo_ns__ = name_space
+        cls.__ooo_enum_name__: str = cls.__get_enum_name()
         cls._initialized = True
 
     def __call__(cls, value) -> uno.Enum:
@@ -138,7 +136,7 @@ class UnoEnumMeta(type):
                 key = __name
                 e = uno.Enum(cls.typeName, __name)
                 # metaclass __dict__ is a mappingproxy
-                # the only way to set attribue is to call super
+                # the only way to set attribute is to call super
                 super().__setattr__(key, e)
                 return cls.__dict__[key]
             except Exception:
@@ -156,8 +154,11 @@ class UnoEnumMeta(type):
             pass
         else:
             # metaclass __dict__ is a mappingproxy
-            # the only way to set attribue is to call super
+            # the only way to set attribute is to call super
             super().__setattr__(key, value)  # Transparent access.
+
+    def __get_enum_name(cls) -> str:
+        return cls.__ooo_full_ns__.rsplit(sep=".", maxsplit=1)[1]
 
 
 class ConstEnumMeta(EnumMeta):
@@ -188,164 +189,51 @@ class ConstEnumMeta(EnumMeta):
         return super().__prepare__(cls, bases)
 
     def __new__(metacls, cls, bases, classdict, **kwds):
+        def set_enum_values(name: str, ns: str, dic: _EnumDict) -> None:
+            m_obj = __import__(name=ns, fromlist=[name])
+            const = getattr(m_obj, name)
+            attrs = [a for a in dir(const) if not a.startswith('__')]
+            for attr in attrs:
+                val = getattr(const, attr, None)
+                if val is not None:
+                    dic[attr] = val
+
+        type_name: str = kwds["type_name"]
+        name_space: str = kwds["name_space"]
+        enum_name = type_name.rsplit(sep=".", maxsplit=1)[1]
+        set_enum_values(enum_name, name_space, classdict)
         return super().__new__(metacls, cls, bases, classdict)
 
     def __init__(cls, name, bases, namespace, **kwds):
         super().__init__(name, bases, namespace)
         type_name = kwds["type_name"]
         name_space = kwds["name_space"]
-        cls.__ooo_full_ns__ = type_name
-        cls.__ooo_ns__ = name_space
+        cls.__ooo_full_ns__: str = type_name
+        cls.__ooo_ns__: str = name_space
+        cls.__ooo_name__: str = cls.__get_enum_name()
+        cls.__ooo_enum_name__: str = cls.__ooo_name__ + "Enum"
         cls._initialized = True
 
-    def __getattr__(cls, name: str) -> uno.Enum | Any:
-        if cls._initialized:
-            # Provide the caller attributes in whatever ways interest you.
-            try:
-                if name.startswith('_'):
-                    try:
-                        super().__setattr__("_initialized", False)
-                        return super().__getattr__(name)
-                    finally:
-                        super().__setattr__("_initialized", True)
-                member = cls._member_map_.get(name, None)
-                if member is None:
-                    try:
-                        super().__setattr__("_initialized", False)
-                        member = cls._create_const_member_(name)
-                    finally:
-                        super().__setattr__("_initialized", True)
-                return member
-            except Exception as e:
-                raise AttributeError(
-                    f"Enum {cls.__ooo_full_ns__} has no attribute {name}") from e
-        else:
-            try:
-                # Transparent access to instance vars.
-                return super().__getattr__(name)
-            except KeyError:
-                raise AttributeError(name)
+    def __call__(cls, value, names=None, *, module=None, qualname=None, type=None, start=1):
+        if isinstance(value, str) and value != cls.__ooo_enum_name__:
+            return cls.__get_enum_from_str(value)
+        return super().__call__(value=value, names=names, module=module, qualname=qualname, type=type, start=start)
 
-    def _create_const_member_(cls, name):
-        const = uno.getConstantByName(f"{cls.__ooo_full_ns__}.{name}")
-        sup = super(cls)
+    def __get_enum_name(cls) -> str:
+        return cls.__ooo_full_ns__.rsplit(sep=".", maxsplit=1)[1]
 
-        # new_enum = sup.__thisclass__(
-        #     sup.__thisclass__.__name__, [(name, const)])
-        enum_dict = _EnumDict()
-        enum_dict._cls_name = cls.__name__
-        enum_dict['_generate_next_value_'] = None
-        enum_dict[name] = const
-        new_enum = cls(
-            sup.__thisclass__.__name__, enum_dict)
-        new_member: Enum = getattr(new_enum, name)
-        # assigning new_member.__classs__ is essential.
-        # Otherwise it will point to the enum in memory just created.
-        # this would lead to other issues as each enum value depends on
-        # having the same class for Flags and other operations.
-        setattr(new_member, "__class__", cls)
-        cls._value2member_map_[new_member.value] = new_member
-        cls._member_map_[new_member.name] = new_member
-
-        cls._member_names_.append(new_member.name)
-        return cls._member_map_[new_member.name]
-
-    if sys.version_info < (3, 8, 0):
-        @staticmethod
-        def _get_mixins_(bases):
-            """Returns the type for creating enum members, and the first inherited
-            enum class.
-
-            bases: the tuple of bases that was given to __new__
-
-            """
-            if not bases:
-                return object, Enum
-
-            def _find_data_type(bases):
-                for chain in bases:
-                    for base in chain.__mro__:
-                        if base is object:
-                            continue
-                        elif '__new__' in base.__dict__:
-                            if issubclass(base, Enum):
-                                continue
-                            return base
-
-            # ensure final parent class is an Enum derivative, find any concrete
-            # data type, and check that Enum has no members
-            first_enum = bases[-1]
-            if not issubclass(first_enum, Enum):
-                raise TypeError("new enumerations should be created as "
-                                "`EnumName([mixin_type, ...] [data_type,] enum_type)`")
-            member_type = _find_data_type(bases) or object
-            return member_type, first_enum
-
-    else:
-        @staticmethod
-        def _get_mixins_(class_name, bases):
-            """
-            Returns the type for creating enum members, and the first inherited
-            enum class.
-
-            bases: the tuple of bases that was given to __new__
-            """
-            if not bases:
-                return object, Enum
-
-            def _find_data_type(bases):
-                data_types = set()
-                for chain in bases:
-                    candidate = None
-                    for base in chain.__mro__:
-                        if base is object:
-                            continue
-                        elif issubclass(base, Enum):
-                            if base._member_type_ is not object:
-                                data_types.add(base._member_type_)
-                                break
-                        elif '__new__' in base.__dict__:
-                            if issubclass(base, Enum):
-                                continue
-                            data_types.add(candidate or base)
-                            break
-                        else:
-                            candidate = candidate or base
-                if len(data_types) > 1:
-                    raise TypeError('%r: too many data types: %r' %
-                                    (class_name, data_types))
-                elif data_types:
-                    return data_types.pop()
-                else:
-                    return None
-
-            # ensure final parent class is an Enum derivative, find any concrete
-            # data type, and check that Enum has no members
-            first_enum = bases[-1]
-            if not issubclass(first_enum, Enum):
-                raise TypeError("new enumerations should be created as "
-                                "`EnumName([mixin_type, ...] [data_type,] enum_type)`")
-            member_type = _find_data_type(bases) or object
-            return member_type, first_enum
-
-    @classmethod
-    def _check_for_existing_members(cls, class_name, bases):
-        if cls._initialized is False:
-            return
-        for chain in bases:
-            for base in chain.__mro__:
-                if issubclass(base, Enum) and base._member_names_:
-                    raise TypeError(
-                        "%s: cannot extend enumeration %r"
-                        % (class_name, base.__name__)
-                    )
+    def __get_enum_from_str(cls, value: str) -> Enum:
+        if not value:
+            return None
+        val = getattr(cls, value, None)
+        return val
 
     def __setattr__(cls, key, value):
         if cls._initialized:
             pass
         else:
             # metaclass __dict__ is a mappingproxy
-            # the only way to set attribue is to call super
+            # the only way to set attribute is to call super
             super().__setattr__(key, value)  # Transparent access.
 
 
@@ -399,7 +287,7 @@ class UnoConstMeta(type):
             try:
                 const = uno.getConstantByName(f"{cls.__ooo_full_ns__}.{name}")
                 # metaclass __dict__ is a mappingproxy
-                # the only way to set attribue is to call super
+                # the only way to set attribute is to call super
                 super().__setattr__(name, const)
                 return cls.__dict__[name]
             except Exception:
@@ -417,5 +305,5 @@ class UnoConstMeta(type):
             pass
         else:
             # metaclass __dict__ is a mappingproxy
-            # the only way to set attribue is to call super
+            # the only way to set attribute is to call super
             super().__setattr__(key, value)  # Transparent access.
